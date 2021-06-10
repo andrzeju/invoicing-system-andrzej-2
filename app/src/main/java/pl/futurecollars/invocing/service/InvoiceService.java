c61de02b70c611dcf2d3deab3d6a9cf724b548bc
andrzej.urban <andrzej.urban@ocado.com>
2021-06-10 06:24:51 +0200

    task-2

---
+++ app/src/main/java/pl/futurecollars/invocing/service/InvoiceService.java
@@ -0,0 +1,36 @@
+package pl.futurecollars.invocing.service;
+
+import java.util.List;
+import java.util.Optional;
+import pl.futurecollars.invocing.db.Database;
+import pl.futurecollars.invocing.model.Invoice;
+
+public class InvoiceService {
+
+    private final Database database;
+
+    public InvoiceService(Database database) {
+        this.database = database;
+    }
+
+    public int save(Invoice invoice) {
+        return database.save(invoice);
+    }
+
+    public Optional<Invoice> getById(int id) {
+        return database.getById(id);
+    }
+
+    public List<Invoice> getAll() {
+        return database.getAll();
+    }
+
+    public void update(int id, Invoice updatedInvoice) {
+        database.update(id, updatedInvoice);
+    }
+
+    public void delete(int id) {
+        database.delete(id);
+    }
+}
+
